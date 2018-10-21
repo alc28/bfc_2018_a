d203eefb85ea7af3243ad81ce76bb192a89f410e
Adam Chandler <adam@zoik.org>
2018-10-21 02:07:01 +0000

    documentation added to sandbox

--- sandbox.R
+++ sandbox.R
@@ -1,4 +1,9 @@
 # sandbox.R
 
 # git config --global credential.helper 'cache --timeout 3600'
-# pushing changes to github needs to be smoother
+
+# rstudio-user@6d45ad597dbf:/cloud/project$ git add .
+# rstudio-user@6d45ad597dbf:/cloud/project$ git commit -m "from terminal"
+# rstudio-user@6d45ad597dbf:/cloud/project$ git status
+# rstudio-user@6d45ad597dbf:/cloud/project$ git push origin master
+
